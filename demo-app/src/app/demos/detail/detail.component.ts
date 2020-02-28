7d7262795f957594545297caca5e0a89e344a57d
Ola Ekdahl <ola@ciracon.com>
2020-02-27 16:12:46 -0800

    update to angular 9

--- demo-app/src/app/demos/detail/detail.component.ts
+++ demo-app/src/app/demos/detail/detail.component.ts
@@ -5,12 +5,13 @@ import { Animations } from '../../shared/animations';
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
-  styleUrls: ['./detail.component.css'],
-  animations: [Animations.page]
+  styleUrls: ['./detail.component.css']
+  // ,animations: [Animations.page]
 })
 export class DetailComponent implements OnInit {
-  @HostBinding('@routeAnimation')
-  anyProperty = 'anything';
+  // @HostBinding('@routeAnimation')
+  // anyProperty = 'anything';
+  
   pid: string;
   constructor(private activeRoute: ActivatedRoute, private router: Router) { }
 
